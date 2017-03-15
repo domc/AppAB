5018899c5e67b08bad2bdae1e0701337331957a7
Domen Perenic <domc987@gmail.com>
2017-03-15 13:11:20 +0100

    Uporabniku dodeljena vloga ob registraciji.

--- AppAB/Controllers/AccountController.cs
+++ AppAB/Controllers/AccountController.cs
@@ -163,6 +163,9 @@ namespace AppAB.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
+                    //Assign Role to user Here      
+                    await UserManager.AddToRoleAsync(user.Id, "user");
+
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
