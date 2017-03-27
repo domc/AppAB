8a6ad8a30013a5d6ecba5f34de39e75a8ee77ec2
Domen Perenic <domc987@gmail.com>
2017-03-27 16:45:25 +0200

    Adding to cart (maybe) finished.

--- AppAB/Controllers/OrdersController.cs
+++ AppAB/Controllers/OrdersController.cs
@@ -19,6 +19,7 @@ namespace AppAB.Controllers
         public const string CartSessionKey = "CartId";
 
         // GET: Orders
+        [Authorize(Roles = "admin")]
         public ActionResult Index()
         {
             var orders = db.orders.Include(o => o.aspnetusers);
@@ -26,46 +27,44 @@ namespace AppAB.Controllers
         }
 
         // GET: Orders/Details/5
+        [Authorize(Roles = "admin,user")]
         public ActionResult Details(string id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
+            //Find orderId(Session) if it exists
+            if (id == "MyCart")
+            {
+                HttpContextBase context = this.HttpContext;
+                if (context.Session[CartSessionKey] == null)
+                {
+                    return View();
+                }
+                else
+                {
+                    id = context.Session[CartSessionKey].ToString();
+                }
+
+            }
             orders orders = db.orders.Find(id);
             if (orders == null)
             {
                 return HttpNotFound();
             }
-            return View(orders);
-        }
-
-        // GET: Orders/Create
-        public ActionResult Create()
-        {
-            ViewBag.user_id = new SelectList(db.aspnetusers, "Id", "Email");
-            return View();
-        }
-
-        // POST: Orders/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,total_price,user_id")] orders orders)
-        {
-            orders.id = "Blaha";
-            if (ModelState.IsValid)
+            else if (System.Web.HttpContext.Current.User.Identity.GetUserId() != orders.user_id)
             {
-                db.orders.Add(orders);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                //If order wasn't made by currently signed user return empty view/cart
+                return View();
             }
-            ViewBag.user_id = new SelectList(db.aspnetusers, "Id", "Email", orders.user_id);
+
             return View(orders);
         }
 
         [HttpPost]
+        [Authorize(Roles = "admin,user")]
         public ActionResult AddToOrder(int? productId)
         {
             if (productId==null)
@@ -91,7 +90,7 @@ namespace AppAB.Controllers
                         guidid = GUIDid.ToString();
                         findOrder = db.orders.Find(guidid);
                     }
-                    while (findOrder != null);                                        
+                    while (findOrder != null);
 
                     //Create new order with selected(added to cart) product
                     orders newOrder = new orders();
@@ -114,65 +113,68 @@ namespace AppAB.Controllers
                 {
                     //Find the order
                     orders order = db.orders.Find(context.Session[CartSessionKey].ToString());
-                    order_items item = db.order_items.Where(i => i.order_id == order.id && i.product_id == product.id).FirstOrDefault();
-                    if (item != null)
+
+                    //If order was made by current user, add product to it, if not, create new order
+                    if (order.user_id == user.Id)
                     {
-                        //Increase quantity if the product is already on the order
-                        item.Quantity += 1;
-                        order.total_price += item.products.price;
+                        order_items item = db.order_items.Where(i => i.order_id == order.id && i.product_id == product.id).FirstOrDefault();
+                        if (item != null)
+                        {
+                            //Increase quantity if the product is already on the order
+                            item.Quantity += 1;
+                            order.total_price += item.products.price;
+                        }
+                        else
+                        {
+                            //Add product to order
+                            item = new order_items();
+                            item.order_id = order.id;
+                            item.product_id = product.id;
+                            item.Quantity = 1;
+
+                            order.order_items.Add(item);
+                            order.total_price += product.price;
+                        }
                     }
                     else
                     {
-                        //Add product to order
-                        item = new order_items();
-                        item.order_id = order.id;
+                        //Generate new GUID
+                        orders findOrder;
+                        string guidid = "";
+                        do
+                        {
+                            Guid GUIDid = Guid.NewGuid();
+                            guidid = GUIDid.ToString();
+                            findOrder = db.orders.Find(guidid);
+                        }
+                        while (findOrder != null);
+
+                        //Create new order with selected(added to cart) product
+                        orders newOrder = new orders();
+                        newOrder.id = guidid;
+                        newOrder.user_id = user.Id;
+                        newOrder.total_price = product.price;
+                        db.orders.Add(newOrder);
+
+                        //dodaj vnos v vmesno tabelo
+                        order_items item = new order_items();
+                        item.order_id = guidid;
                         item.product_id = product.id;
                         item.Quantity = 1;
+                        db.order_items.Add(item);
 
-                        order.order_items.Add(item);
-                        order.total_price += product.price;
-                    }                    
+                        //Set cookie
+                        context.Session[CartSessionKey] = guidid;
+                    }            
                 }
 
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Details", new { id = "MyCart" });
             }
-        }
-
-        // GET: Orders/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            orders orders = db.orders.Find(id);
-            if (orders == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.user_id = new SelectList(db.aspnetusers, "Id", "Email", orders.user_id);
-            return View(orders);
-        }
-
-        // POST: Orders/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,total_price,user_id")] orders orders)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(orders).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.user_id = new SelectList(db.aspnetusers, "Id", "Email", orders.user_id);
-            return View(orders);
         }
 
         // GET: Orders/Delete/5
+        [Authorize(Roles = "admin")]
         public ActionResult Delete(string id)
         {
             if (id == null)
@@ -190,6 +192,7 @@ namespace AppAB.Controllers
         // POST: Orders/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
         public ActionResult DeleteConfirmed(string id)
         {
             orders orders = db.orders.Find(id);
